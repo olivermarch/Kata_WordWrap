08249f29f870c6ef35edda265241e8f6e2dad08e
Oliver March <march89@gmail.com>
2022-04-18 14:26:42 +0100

    replace spaces by break lines

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -1,14 +1,17 @@
 public class WordWrap{
 
+    static String lineBreak = "\n";
+
     public String wrap(String texToWrap, int maxWidth) {
 
-        if (texToWrap.length() < maxWidth) {
+        if (texToWrap.length() <= maxWidth) {
 
             return texToWrap;
-
         } else{
-        return null;
+            if (texToWrap.indexOf(" ") != -1) {
+                return texToWrap.replace(" ", "\n");
+            }
+       return texToWrap.substring(0, maxWidth) + lineBreak + texToWrap.substring(maxWidth);
         }
-
     }
 }
--- src/test/java/WordWrapShould.java
+++ src/test/java/WordWrapShould.java
@@ -6,14 +6,26 @@ public class WordWrapShould {
 
     /*
     TODO list
-    ("hola", 7) -> "hola"
+    "hola", 7 -> "hola"
+    "hola", 2 -> "ho\nla"
+    "holaho", 2 -> "ho\nla\nho"
+    "hola fulano", 7 -> "hola\nfulano"
     */
     WordWrap wordWrap = new WordWrap();
 
     @Test
     public void wrap_one_word(){
-
         assertEquals("hola", wordWrap.wrap("hola",7));
     }
+    @Test
+    public void split_one_word(){
+        assertEquals("ho\nla", wordWrap.wrap("hola",2));
+
+    }
+    @Test
+    public void replace_spaces_by_break_lines(){
+        assertEquals("hola\nfulano", wordWrap.wrap("hola fulano",7));
+    }
+
 
 }
